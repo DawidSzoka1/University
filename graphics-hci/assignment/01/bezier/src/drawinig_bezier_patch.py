349e6b78fdd4a8a9b8c68c9ffe4fcb1e210d26b1
DawidSzoka1 <oficer130@gmail.com>
2025-03-06 14:55:07 +0100

    some small changes

--- graphics-hci/assignment/01/bezier/src/drawinig_bezier_patch.py
+++ graphics-hci/assignment/01/bezier/src/drawinig_bezier_patch.py
@@ -2,13 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 from read_control_points import read_control_points_from_txt
+
 # Ustawienie backendu interaktywnego
 matplotlib.use('TkAgg')
 plt.ion()
 
+
 def bernstein(n, i, t):
     from math import comb
     return comb(n, i) * (t ** i) * ((1 - t) ** (n - i))
+
 
 def bezier_patch(control_points):
     resolution = 20
@@ -22,17 +25,18 @@ def bezier_patch(control_points):
                 for j in range(4):
                     b_u = bernstein(3, i, u)
                     b_w = bernstein(3, j, w)
-                    index = i * 4 + j  # Poprawione indeksowanie
+                    index = i * 4 + j
                     px += control_points[index][0] * b_u * b_w
                     py += control_points[index][1] * b_u * b_w
                     pz += control_points[index][2] * b_u * b_w
             surface.append([px, py, pz])
     return np.array(surface)
 
+
 def plot_teapot(control_points):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    for k in range(32):
+    for k in range(control_points.shape[0]):
         surface = bezier_patch(control_points[k])
         ax.scatter(surface[:, 0], surface[:, 1], surface[:, 2], s=1, color='b')
     ax.set_xlabel("X")
@@ -42,6 +46,7 @@ def plot_teapot(control_points):
     ax.set_box_aspect([1, 1, 1])  # Utrzymanie równych proporcji osi
     plt.show(block=True)  # Interaktywny widok umożliwiający obracanie myszką
 
-file_path = "teapotCGA.bpt.txt"  # Ścieżka do pliku BPT z punktami
+
+file_path = "teapotCGA.bpt.txt"  # Ścieżka do pliku z punktami
 control_points = read_control_points_from_txt(file_path, (32, 16, 3))
 plot_teapot(control_points)
