e689186df96fb4fbe4e97311ca905716415e55a4
DawidSzoka1 <oficer130@gmail.com>
2025-04-02 21:46:17 +0200

    data set modification for project

--- statistics-r/notebooks/lab03.R
+++ statistics-r/notebooks/lab03.R
@@ -1,21 +1,24 @@
 library(tidyverse)
 library(moments)
+library(e1071)
 
 set.seed(20250322)
 x <- rnorm(1000, mean = 7, sd = 2)
+# na.rm = TRUE usuwa braki danych 
+moments::moment(x, order = 2, na.rm=TRUE)
+moments::moment(x, order = 2, absolute = TRUE, na.rm=TRUE)
+moments::moment(x, order = 2, central =  TRUE)
+moments::moment(x, order = 2, central = TRUE, absolute = TRUE, na.rm=TRUE)
 
-moment(x, order = 2)
-moment(x, order = 2, absolute = TRUE)
-moment(x, order = 2, central = TRUE)
-moment(x, order = 2, central = TRUE, absolute = TRUE)
-
-moment(x, order = 3)
-moment(x, order = 3, absolute = TRUE)
-moment(x, order = 3, central = TRUE)
-moment(x,  order = 3, central = TRUE, absolute = TRUE)
+moments::moment(x, order = 3, na.rm=TRUE)
+moments::moment(x, order = 3, absolute = TRUE, na.rm=TRUE)
+moments::moment(x, order = 3, central = TRUE, na.rm=TRUE)
+moments::moment(x,  order = 3, central = TRUE, absolute = TRUE, na.rm=TRUE)
 
 help("all.moments")
 all.moments(x, order.max = 3)
 all.moments(x, order.max = 3, absolute = TRUE)
 all.moments(x, order.max = 3, central = TRUE)
 all.moments(x, order.max = 3, central = TRUE, absolute = TRUE)
+
+R.version.string
---
+++ statistics-r/projeckt/dataSetModifications.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+
+vehicles
+fuel_sample = vehicles[sample(nrow(vehicles), 2000),]
+fuel_sample
+
+colnames(fuel_sample)[colnames(fuel_sample) == "city08"] <- "city_mpg"
+colnames(fuel_sample)[colnames(fuel_sample) == "highway08"] <- "highway_mpg"
+colnames(fuel_sample)[colnames(fuel_sample) == "comb08"] <- "comb_mpg"
+
+write.csv(fuel_sample, "fuel_sample.csv", row.names = FALSE)
+
+shapiro.test(sample(fuel_sample$city_mpg, 20))
+
+
+fuel_sample$fuel_type <- factor(fuel_sample$fuel_type, 
+                              levels = c("Electric", "Hybrid", "Gasoline", "Diesel"), 
+                              ordered = TRUE)
+
+colnames(fuel_sample)
+table(fuel_sample$fuelType1)
+is.factor(fuel_sample$VClass)
+unique(fuel_sample$VClass)
+
+
+fuel_data <- read.csv("fuel_sample.csv", stringsAsFactors = TRUE)
+
+is.factor(fuel_data$VClass)
+is.ordered(fuel_data$VClass)
+fuel_data <- fuel_data %>%
+  mutate(across(c(VClass, make, model, fuelType, fuelType1, drive, trany, atvType, guzzler, mpgData), as.factor))
+is.ordered(fuel_data$mpgData)
+unique(fuel_data$mpgData)
