6e40f60505d88d3071ca96dbb4271b9b48280acf
DawidSzoka1 <oficer130@gmail.com>
2025-04-13 17:56:14 +0200

    html almost done

--- web-programing/projectHtml/js/findGame.js
+++ web-programing/projectHtml/js/findGame.js
@@ -1,8 +1,11 @@
 addEventListener("DOMContentLoaded", () => {
     const find = document.getElementById("findGame")
     find.addEventListener("click", (e) => {
+        e.preventDefault()
         document.getElementById("searching").classList.remove("d-none");
         document.getElementById("lobby-info").classList.add("d-none")
-        e.preventDefault()
+        setTimeout(() =>{
+            window.location.href = "/projectHtml/game.html"
+        }, 4000)
     })
 })
