5b9beb8e340f00fda643a555b5834212e3e21cbf
DawidSzoka1 <oficer130@gmail.com>
2025-04-24 18:23:44 +0200

    added Input dialog to pass amount to add or subtraction for line transformation plus convert image type changed to RGB888

--- graphics-hci/assignment/02/convertImage.py
+++ graphics-hci/assignment/02/convertImage.py
@@ -2,16 +2,16 @@ import numpy as np
 from PyQt5.QtGui import QImage, QPixmap
 
 def qpixmap_to_numpy(pixmap):
-    image = pixmap.toImage().convertToFormat(QImage.Format_RGBA8888)
+    image = pixmap.toImage().convertToFormat(QImage.Format_RGB888)
     width = image.width()
     height = image.height()
     ptr = image.bits()
     ptr.setsize(image.byteCount())
-    arr = np.array(ptr, dtype=np.uint8).reshape((height, width, 4))
+    arr = np.array(ptr, dtype=np.uint8).reshape((height, width, 3))
     return arr.copy()
 
 def numpy_to_qpixmap(arr):
     h, w, ch = arr.shape
     bytes_per_line = ch * w
-    image = QImage(arr.data, w, h, bytes_per_line, QImage.Format_RGBA8888)
+    image = QImage(arr.data, w, h, bytes_per_line, QImage.Format_RGB888)
     return QPixmap.fromImage(image.copy())
--- graphics-hci/assignment/02/lineTransformation.py
+++ graphics-hci/assignment/02/lineTransformation.py
@@ -1,13 +1,19 @@
 import numpy as np
 from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QDialog, QInputDialog, QMessageBox
+
 from convertImage import qpixmap_to_numpy, numpy_to_qpixmap
+
 
 class LineTransformation:
     def __init__(self, image_transformation):
         self.image_transformation = image_transformation
 
-
-    def brightness(self, pixmap, amount=50):
+    def brightness(self, pixmap, frame):
+        amount, ok1 = QInputDialog.getInt(frame, "Rozjaśnienie", "moc", 20, 0, 200, 1)
+        if not ok1:
+            QMessageBox.warning(frame, "Błąd", "Nie podano poprawnych wartości rozjaśnienia")
+            return
         arr = qpixmap_to_numpy(pixmap)
         for i in range(3):  # R, G, B
             arr[..., i] = np.clip(arr[..., i].astype(np.int16) + amount, 0, 255).astype(np.uint8)
@@ -15,7 +21,11 @@ class LineTransformation:
         result = numpy_to_qpixmap(arr)
         self.image_transformation.setPixmap(result)
 
-    def darken(self, pixmap, amount=50):
+    def darken(self, pixmap, frame):
+        amount, ok1 = QInputDialog.getInt(frame, "Przyciemnienie", "moc", 20, 0, 200, 1)
+        if not ok1:
+            QMessageBox.warning(frame, "Błąd", "Nie podano poprawnych wartości przyciemnienia")
+            return
         arr = qpixmap_to_numpy(pixmap)
         for i in range(3):  # R, G, B
             arr[..., i] = np.clip(arr[..., i].astype(np.int16) - amount, 0, 255).astype(np.uint8)
