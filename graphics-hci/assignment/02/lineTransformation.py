84e34e17b62f575c696d59e668d04481a6dab0cd
DawidSzoka1 <oficer130@gmail.com>
2025-04-24 11:31:11 +0200

    utils file for image transformation form QPixmap to np array and revers plus fixed error with some wrong rgb values in line transformation

--- graphics-hci/assignment/02/lineTransformation.py
+++ graphics-hci/assignment/02/lineTransformation.py
@@ -1,41 +1,31 @@
 import numpy as np
 from PyQt5.QtGui import QImage, QPixmap
+from convertImage import qpixmap_to_numpy, numpy_to_qpixmap
 
 class LineTransformation:
     def __init__(self, image_transformation):
         self.image_transformation = image_transformation
 
-    def qpixmap_to_numpy(self, pixmap):
-        image = pixmap.toImage().convertToFormat(QImage.Format_RGBA8888)
-        width = image.width()
-        height = image.height()
-        ptr = image.bits()
-        ptr.setsize(image.byteCount())
-        arr = np.array(ptr, dtype=np.uint8).reshape((height, width, 4))
-        return arr.copy()
-
-    def numpy_to_qpixmap(self, arr):
-        h, w, ch = arr.shape
-        bytes_per_line = ch * w
-        image = QImage(arr.data, w, h, bytes_per_line, QImage.Format_RGBA8888)
-        return QPixmap.fromImage(image.copy())
 
     def brightness(self, pixmap, amount=50):
-        arr = self.qpixmap_to_numpy(pixmap)
+        arr = qpixmap_to_numpy(pixmap)
         for i in range(3):  # R, G, B
-            arr[..., i] = np.clip(arr[..., i] + amount, 0, 255)
-        result = self.numpy_to_qpixmap(arr)
+            arr[..., i] = np.clip(arr[..., i].astype(np.int16) + amount, 0, 255).astype(np.uint8)
+
+        result = numpy_to_qpixmap(arr)
         self.image_transformation.setPixmap(result)
 
     def darken(self, pixmap, amount=50):
-        arr = self.qpixmap_to_numpy(pixmap)
+        arr = qpixmap_to_numpy(pixmap)
         for i in range(3):  # R, G, B
-            arr[..., i] = np.clip(arr[..., i] - amount, 0, 255)
-        result = self.numpy_to_qpixmap(arr)
+            arr[..., i] = np.clip(arr[..., i].astype(np.int16) - amount, 0, 255).astype(np.uint8)
+
+        result = numpy_to_qpixmap(arr)
         self.image_transformation.setPixmap(result)
 
     def negative(self, pixmap):
-        arr = self.qpixmap_to_numpy(pixmap)
-        arr[..., :3] = 255 - arr[..., :3]  # tylko R, G, B
-        result = self.numpy_to_qpixmap(arr)
+        arr = qpixmap_to_numpy(pixmap)
+        for i in range(3):  # R, G, B
+            arr[..., i] = np.clip(255 - arr[..., i].astype(np.int16), 0, 255).astype(np.uint8)
+        result = numpy_to_qpixmap(arr)
         self.image_transformation.setPixmap(result)
--- graphics-hci/assignment/02/powerTransformation.py
+++ graphics-hci/assignment/02/powerTransformation.py
@@ -1,32 +1,17 @@
 import numpy as np
 from PyQt5.QtGui import QPixmap, QImage
+from convertImage import qpixmap_to_numpy, numpy_to_qpixmap
 
 class PowerTransformation:
     def __init__(self, image_transformation):
         self.image_transformation = image_transformation
-
-    def qpixmap_to_numpy(self, pixmap):
-        image = pixmap.toImage().convertToFormat(QImage.Format_RGBA8888)
-        width = image.width()
-        height = image.height()
-        ptr = image.bits()
-        ptr.setsize(image.byteCount())
-        arr = np.array(ptr, dtype=np.uint8).reshape((height, width, 4))
-        return arr.copy()
-
-    def numpy_to_qpixmap(self, arr):
-        h, w, ch = arr.shape
-        bytes_per_line = ch * w
-        image = QImage(arr.data, w, h, bytes_per_line, QImage.Format_RGBA8888)
-        return QPixmap.fromImage(image.copy())
-
     def power_transformation(self, pixmap, gamma):
-        np_img = self.qpixmap_to_numpy(pixmap)
+        np_img = qpixmap_to_numpy(pixmap)
 
         for i in range(3):  # R, G, B
             np_img[..., i] = np.clip((np_img[..., i] / 255.0) ** gamma * 255, 0, 255).astype(np.uint8)
 
-        transformed = self.numpy_to_qpixmap(np_img)
+        transformed = numpy_to_qpixmap(np_img)
         self.image_transformation.setPixmap(transformed)
 
     def brightness(self, pixmap, gamma=0.5):
