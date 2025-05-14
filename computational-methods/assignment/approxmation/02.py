1a409aa520e334ab54e996fbea3493ff2fbc8253
DawidSzoka1 <oficer130@gmail.com>
2025-05-14 11:40:15 +0200

    final version of second method with example from lab

--- computational-methods/assignment/approxmation/02.py
+++ computational-methods/assignment/approxmation/02.py
@@ -1,9 +1,8 @@
 import numpy as np
 
-x_values = [1, 2, 3, 4]
-y_values = [6, 19, 40, 69]
-n = 4
-m = 2
+x_values = [-1, -0.5, 0, 0.5, 1]
+n = 10
+m = 4
 
 
 def calculate_aij(x_values, i, j):
@@ -13,27 +12,32 @@ def calculate_aij(x_values, i, j):
     return result
 
 
-
-b_array = np.zeros((m + 1, 1))
-for i in range(m + 1):
-    result = []
-    for j in range(n):
-        result.append(x_values[j] ** i * y_values[j])
-    sum = 0
-    for x in result:
-        sum += x
-    b_array[i][0] = sum
-
-a_array = np.zeros((m + 1, m + 1))
-for i in range(m + 1):
-    for j in range(m + 1):
-        a_array[i, j] = calculate_aij(x_values, i, j)
-result = np.linalg.solve(a_array, b_array)
-
-def calculate_wx(x, n, values):
+def calculate_bk(x_values, y_values, k):
     result = 0
-    for i in range(n):
-        result += values[i] * x ** i
+    for i in range(len(x_values)):
+        result += (x_values[i] ** k) * y_values[i]
     return result
 
-print(calculate_wx(2.5, 3, result))
+
+def function(x):
+    return (x ** 3 - 2 * x + 10) ** (1 / 2)
+
+
+def calculate_wx(look_for_value, x_values, n, m, func=function):
+    y_values = [func(x) for x in x_values]
+    b_array = np.zeros((m + 1, 1))
+    for i in range(m + 1):
+        b_array[i][0] = calculate_bk(x_values, y_values, i)
+
+    a_array = np.zeros((m + 1, m + 1))
+    for i in range(m + 1):
+        for j in range(m + 1):
+            a_array[i, j] = calculate_aij(x_values, i, j)
+    values = np.linalg.solve(a_array, b_array)
+    result = 0
+    for i in range(m):
+        result += values[i][0] * look_for_value ** i
+    return result
+
+
+print(calculate_wx(0.4, x_values, n, m))
