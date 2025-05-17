9f3976893d1eacfcc65d7ceeb1042a0a77536129
DawidSzoka1 <oficer130@gmail.com>
2025-05-17 15:15:52 +0200

    small changes

--- computational-methods/assignment/approxmation/03.py
+++ computational-methods/assignment/approxmation/03.py
@@ -1,20 +1,20 @@
 import numpy as np
 
 x_values = [-1, -0.5, 0, 0.5, 1]
-n = 10
+n = 3
 m = 5
 
 
-def calculate_aij(x_values, i, j):
+def calculate_sij(x_values, i, j, m):
     result = 0
-    for x in x_values:
-        result += x ** (i + j)
+    for s in range(m):
+        result += x_values[s] ** (i + j)
     return result
 
 
-def calculate_bk(x_values, y_values, k):
+def calculate_tk(x_values, y_values, k, m):
     result = 0
-    for i in range(len(x_values)):
+    for i in range(m):
         result += (x_values[i] ** k) * y_values[i]
     return result
 
@@ -25,17 +25,17 @@ def function(x):
 
 def calculate_wx(look_for_value, x_values, n, m, func=function):
     y_values = [func(x) for x in x_values]
-    b_array = np.zeros((m + 1, 1))
-    for i in range(m + 1):
-        b_array[i][0] = calculate_bk(x_values, y_values, i)
+    b_array = np.zeros((n + 1, 1))
+    for i in range(n + 1):
+        b_array[i][0] = calculate_tk(x_values, y_values, i, m)
 
-    a_array = np.zeros((m + 1, m + 1))
-    for i in range(m + 1):
-        for j in range(m + 1):
-            a_array[i, j] = calculate_aij(x_values, i, j)
+    a_array = np.zeros((n + 1, n + 1))
+    for i in range(n + 1):
+        for j in range(n + 1):
+            a_array[i, j] = calculate_sij(x_values, i, j, m)
     values, *_ = np.linalg.lstsq(a_array, b_array, rcond=None)
     result = 0
-    for i in range(m+1):
+    for i in range(n+1):
         result += values[i][0] * look_for_value ** i
     return result
 
