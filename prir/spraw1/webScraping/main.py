757b7eb814cf917caa0450cda49b33c6c72769fa
DawidSzoka1 <oficer130@gmail.com>
2025-11-12 10:45:52 +0100

    web scraping analyze files with plots

--- prir/spraw1/webScraping/main.py
+++ prir/spraw1/webScraping/main.py
@@ -77,10 +77,67 @@ def download_parallel(urls):
 
 
 
+def analyze_json():
+    results = []
+    files = []
+    for f in os.listdir('.'):
+        if not f.endswith('.json'):
+            continue
+        if f[:6] not in files:
+            files.append(f[:6])
+    for file in files:
+        print(f"Analyzing {file}")
+        with open(f'{file}_seq.json', 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            text = data.get("text").lower()
+            word_count = len(re.findall(r"\w+", text))
+        char_count = len(text)
+        keyword_count = {kw: text.count(kw.lower()) for kw in KEYWORDS}
+        results.append({
+            "url": data.get("url", ""),
+            "filename": file,
+            "chars": char_count,
+            "words": word_count,
+            **keyword_count
+        })
+    return results
+
+
+def plot_analysis(results):
+    files = [r['filename'] for r in results]
+    word_count = [r['words'] for r in results]
+    chars = [r['chars'] for r in results]
+    plt.figure(figsize=(8,4))
+    plt.bar(files, chars, color='orange', label='znaki')
+    plt.bar(files, word_count, color='green', alpha=0.6, label='słowa')
+    plt.title('Liczba znaków i słów w plikach')
+    plt.ylabel('Liczba')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(f"chars_words_count.png")
+    plt.show()
+    for kw in KEYWORDS:
+        plt.figure(figsize=(12, 6))
+        plt.bar(files, [r[kw] for r in results], color='skyblue')
+        plt.title(f"Wystapienia slowa '{kw}'")
+        plt.ylabel("liczba wystapien")
+        plt.tight_layout()
+        plt.savefig(f"files_{kw}.png")
+        plt.show()
+
 if __name__ == "__main__":
     time_seq = download_sequential(URLS)
     time_parallel = download_parallel(URLS)
 
+    plt.bar(['Bez wątków', 'Z wątkami'],[time_seq, time_parallel], color=['gray', 'green'])
+    plt.title('Prównanie czasu pobierania')
+    plt.ylabel('Czas [s]')
+    plt.savefig('time_compare.png')
+    plt.show()
+
     print(f"Czas bez wielowątkowości: {time_seq:.2f} s")
     print(f"Czas z wielowątkowością: {time_parallel:.2f} s")
+
+    results = analyze_json()
+    plot_analysis(results)
 
