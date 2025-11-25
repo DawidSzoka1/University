gradient method plus newton fix

optimalization-methods/multiUnknowFunctions/myFunction.py
@@ -30,8 +30,8 @@ def functionPochxy(x, y):
     return -1
 
 
-h = 0.00001
-round_spot = 4
+h = 0.000001
+round_spot = 10
 
 
 def dfdx(f, x, y):
@@ -52,6 +52,58 @@ def dfdydy(f, x, y):
 
 def dfdxdy(f, x, y):
     return round((f(x + h, y + h) - f(x + h, y) - f(x, y + h) + f(x, y)) / (h ** 2), round_spot)
+
+
+def example_function(x, y):
+    return 10 * x ** 2 + 12 * x * y + 10 * y ** 2
+
+
+def testFunctionPochxy(x, y):
+    return 12
+
+
+def testFunctionPochxx(x, y):
+    return 20
+
+
+def testFUnctionPochyy(x, y):
+    return 20
+
+
+def testFunctionPochx(x, y):
+    return 20 * x + 12 * y
+
+
+def testFunctionPochy(x, y):
+    return 12 * x + 20 * y
+
+
+def gradient(x, y, pochx=functionPochx, pochy=functionPochy):
+    return np.array([pochx(x, y), pochy(x, y)])
+
+
+def gradientF(x, y, fun=function):
+    return np.array([dfdx(fun, x, y), dfdy(fun, x, y)])
+
+
+def hassego(x, y, func=None, pochxx=functionPoch2x, pochyy=functionPoch2y, pochxy=functionPochxy):
+    if func:
+        return np.array(
+            [[pochxx(func, x, y), pochxy(func, x, y)],
+             [pochxy(func, x, y), pochyy(func, x, y)]])
+    return np.array(
+        [[pochxx(x, y), pochxy(x, y)],
+         [pochxy(x, y), pochyy(x, y)]])
+
+
+def calculate_alpha(grad, hess):
+    a = grad[0]
+    b = grad[1]
+    c = hess[0][0]
+    d = hess[0][1]
+    e = hess[1][0]
+    f = hess[1][1]
+    return (a ** 2 + b ** 2) / ((a * c + b * e) * a + (a * d + b * f) * b)
 
 
 if __name__ == '__main__':

optimalization-methods/multiUnknowFunctions/newton.py
@@ -5,40 +5,18 @@ x0 = 2
 y0 = 2
 
 
-def testFunctionPochxy(x, y):
-    return 12
-
-
-def testFunctionPochxx(x, y):
-    return 20
-
-
-def testFUnctionPochyy(x, y):
-    return 20
-
-
-def testFunctionPochx(x, y):
-    return 20 * x + 12 * y
-
-
-def testFunctionPochy(x, y):
-    return 12 * x + 20 * y
-
-
-
 def calculate(x0, y0, e, function=function, pochxy=functionPochxy, pochx=functionPochx, pochy=functionPochy,
               poch2x=functionPoch2x, poch2y=functionPoch2y, iterations=100):
     xk, yk = x0, y0
     for i in range(iterations):
-        gradient = np.array([pochx(xk, yk), pochy(xk, yk)])
-        hessego = np.array(
-            [[poch2x(xk, yk), pochxy(xk, yk)], [pochxy(xk, yk), poch2y(xk, yk)]])
+        grad = gradient(xk, yk, pochx, pochy)
+        hasse = hassego(xk, yk, pochxx=poch2x, pochyy=poch2y, pochxy=pochxy)
 
-        xk2, yk2 = np.array([xk, yk]) - np.dot(reversed_matrix(hessego), gradient)
+        xk2, yk2 = np.array([xk, yk]) - np.dot(reversed_matrix(hasse), grad)
         print(
-            f"iteracja {i + 1}: gradtient: \n{gradient}\nmacierz hessego: \n{hessego}\nx_{i + 1}={xk2}, y_{i + 1}={yk2}")
-        gradient_k2 = np.array([pochx(xk2, yk2), pochy(xk2, yk2)])
-        if np.linalg.norm(gradient_k2) <= e or (abs(xk2 - xk) <= e and abs(yk2 - yk) <= e):
+            f"iteracja {i + 1}: gradtient: \n{grad}\nmacierz hessego: \n{hasse}\nx_{i + 1}={xk2}, y_{i + 1}={yk2}")
+        grad_k2 = gradient(xk2, yk2, pochx, pochy)
+        if np.linalg.norm(grad_k2) <= e or (abs(xk2 - xk) <= e and abs(yk2 - yk) <= e):
             print()
             return xk2, yk2, i + 1
         xk, yk = xk2, yk2

optimalization-methods/multiUnknowFunctions/newtonv2.py
@@ -5,22 +5,16 @@ x0 = 2
 y0 = 2
 
 
-def testFunction(x, y):
-    return 10 * x ** 2 + 12 * x * y + 10 * y ** 2
-
-
 def calculate(x0, y0, e, function=function, pochxy=dfdxdy, pochx=dfdx, pochy=dfdy,
               poch2x=dfdxdx, poch2y=dfdydy, iterations=100):
     xk, yk = x0, y0
     for i in range(iterations):
-        gradient = np.array([pochx(function, xk, yk), pochy(function, xk, yk)])
-        hessego = np.array(
-            [[poch2x(function, xk, yk), pochxy(function, xk, yk)],
-             [pochxy(function, xk, yk), poch2y(function, xk, yk)]])
-        xk2, yk2 = np.array([xk, yk]) - np.dot(reversed_matrix(hessego), gradient)
+        grad = gradientF(xk, yk, function)
+        hess = hassego(xk, yk, func=function, pochxx=poch2x, pochyy=poch2y, pochxy=pochxy)
+        xk2, yk2 = np.array([xk, yk]) - np.dot(reversed_matrix(hess), grad)
         print(
-            f"iteracja {i + 1}: gradtient: \n{gradient}\nmacierz hessego: \n{hessego}\nx_{i + 1}={xk2}, y_{i + 1}={yk2}")
-        gradient_k2 = np.array([pochx(function, xk2, yk2), pochy(function, xk2, yk2)])
+            f"iteracja {i + 1}: gradtient: \n{grad}\nmacierz hessego: \n{hess}\nx_{i + 1}={xk2}, y_{i + 1}={yk2}")
+        gradient_k2 = gradientF(xk2, yk2, function)
         if np.linalg.norm(gradient_k2) <= e or (abs(xk2 - xk) <= e and abs(yk2 - yk) <= e):
             return xk2, yk2, i + 1
         xk, yk = xk2, yk2
@@ -28,5 +22,5 @@ def calculate(x0, y0, e, function=function, pochxy=dfdxdy, pochx=dfdx, pochy=dfd
 
 
 if __name__ == "__main__":
-    print(calculate(2, 2, 0.01 ))
-    print(calculate(10, 12, 0.01, function=testFunction))
+    print(calculate(10, 12, 0.01, function=example_function))
+    print(calculate(2, 2, 0.01))